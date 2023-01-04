19e44e1b75097609b27681aef9a40bd3917e33ad
diMASasin <35368058+diMASasin@users.noreply.github.com>
2023-01-04 22:06:00 +0500

    xz

--- Logger/Logger/Program.cs
+++ Logger/Logger/Program.cs
@@ -1,12 +1,102 @@
 ﻿using System;
+using System.IO;
 
 namespace Logger
 {
+    interface ILogger
+    {
+        public void WriteError(string message);
+    }
+
+    class ConsoleLogWritter : ILogger
+    {
+        ILogger _logger;
+
+        public ConsoleLogWritter(ILogger logger = null)
+        {
+            _logger = logger;
+        }
+
+        public void WriteError(string message)
+        {
+            Console.WriteLine(message);
+
+            if(_logger != null)
+                _logger.WriteError(message);
+        }
+    }
+
+    class FileLogWritter : ILogger
+    {
+        ILogger _logger;
+
+        public FileLogWritter(ILogger logger = null)
+        {
+            _logger = logger;
+        }
+
+        public virtual void WriteError(string message)
+        {
+            File.WriteAllText("log.txt", message);
+
+            if (_logger != null)
+                _logger.WriteError(message);
+        }
+    }
+
+    class SecureConsoleLogWritter : ILogger
+    {
+        ILogger _logger;
+
+        public SecureConsoleLogWritter(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void WriteError(string message)
+        {
+            if (DateTime.Now.DayOfWeek == DayOfWeek.Friday)
+            {
+                _logger.WriteError(message);
+            }
+        }
+    }
+
+    class Pathfinder : ILogger
+    {
+        ILogger _logger;
+
+        public Pathfinder(ILogger logger)
+        {
+            _logger = logger;
+        }
+         
+        public void Find()
+        {
+            
+        }
+
+        public void WriteError(string message)
+        {
+            _logger.WriteError(message);
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Pathfinder pathfinder = new Pathfinder(new ConsoleLogWritter());
+            Pathfinder pathfinder1 = new Pathfinder(new FileLogWritter());
+            Pathfinder pathfinder2 = new Pathfinder(new SecureConsoleLogWritter(new ConsoleLogWritter()));
+            Pathfinder pathfinder3 = new Pathfinder(new SecureConsoleLogWritter(new FileLogWritter()));
+            Pathfinder pathfinder4 = new Pathfinder(new ConsoleLogWritter(new SecureConsoleLogWritter(new FileLogWritter())));
+            pathfinder.WriteError("adsda");
+            pathfinder1.WriteError("hjm");
+            pathfinder2.WriteError("www");
+            pathfinder3.WriteError("aaa");
+            pathfinder4.WriteError("zzz");
         }
     }
 }
+
